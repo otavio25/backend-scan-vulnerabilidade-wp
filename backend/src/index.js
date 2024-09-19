224ad1da0b33b86461be4e3c218bf8419ee87023
otavio25 <souzaotavio721@gmail.com>
2024-09-19 18:46:08 -0300

    refactor: Adiciona cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const router = require('./router/router')
+const cors = require('cors')
 
+app.use(cors({
+    //origin: 'http://localhost:4200', // Permite apenas essa origem
+    methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+}));
 app.use(express.json())
 app.use(router)
 
